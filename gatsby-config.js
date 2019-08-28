4a72b31352777554b225bdbea517c4315cdcd389
Josue Perez <che.perez3@gmail.com>
2019-08-28 16:30:03 -0400

    add QuizController component

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,14 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    `gatsby-plugin-netlify-cms`
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
 }
